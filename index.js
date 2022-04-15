0efe7762a06473fd8f8b35d1c4ec9a1ad0a09a01
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-04-15 07:54:31 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// this makes a variable that selects the element
+var paragraph = document.getElementById("paragraph");
+// this is the function that logs a peice of text when my selected element is copied
+function copyP() {
+    console.log("It was copied!!! This function worked!!")
+}
+// this adds the event listener to my element and tells the function to run my function when the event is oberved
+paragraph.addEventListener("copy", copyP)
